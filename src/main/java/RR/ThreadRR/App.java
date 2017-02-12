ee70bcd9189e63af02873d5d0934a94ceae5d7a4
Raczkowski <raczkowskir@yahoo.com>
2017-02-12 16:21:38 +0100

    first commit

---
+++ src/main/java/RR/ThreadRR/App.java
@@ -0,0 +1,13 @@
+package RR.ThreadRR;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
